829753d308c8367cdd1a23beb0f80c2bb1ca4d1f
seongWon <tjddnjs8503@naver.com>
2025-04-27 00:23:39 +0900

    no message

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -5,9 +5,9 @@ import { authOptions } from '../../auth/[...nextauth]/auth';
 
 export async function GET(
   request: NextRequest,
-  context: { params?: Record<string, string | string[]> }
+  context: { params: Record<string, string | string[]> }
 ) {
-  const id = context.params?.id as string;
+  const id = context.params.id;
   try {
     const post = await prisma.post.findUnique({
       where: { id },
@@ -26,9 +26,9 @@ export async function GET(
 
 export async function DELETE(
   request: NextRequest,
-  context: { params?: Record<string, string | string[]> }
+  context: { params: Record<string, string | string[]> }
 ) {
-  const id = context.params?.id as string;
+  const id = context.params.id;
   try {
     const session = await getServerSession(authOptions);
     if (!session?.user?.id) {
@@ -50,9 +50,9 @@ export async function DELETE(
 
 export async function PUT(
   request: NextRequest,
-  context: { params?: Record<string, string | string[]> }
+  context: { params: Record<string, string | string[]> }
 ) {
-  const id = context.params?.id as string;
+  const id = context.params.id;
   try {
     const session = await getServerSession(authOptions);
     if (!session?.user?.id) {
